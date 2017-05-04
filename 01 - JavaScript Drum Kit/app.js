13201e8334ea99648210ece858650015ae0102f6
Buki <Bukithompson@gmail.com>
2017-05-04 15:52:57 +0100

    working with event listeners

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const key = document.querySelector('key');
+  key.addEventListener('click', () => {
+    console.log('data-key');
+  });
+
+});
